eee2c169679e4e98e4b6c4364f411ba34bdb2bec
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-03-22 15:15:08 +0100

    aktualisiertes CSS, alle Karten am Anfang umgedreht

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./App.css";
 // Array: Alle Karten
 const cards = [
   // Einzelnes Objekt beschreibt eine Karte
-  { id: 1, color: "#f3722c", label: "Orange", display: true },
-  { id: 2, color: "#f3722c", label: "Orange", display: true },
+  { id: 1, color: "#f3722c", label: "Orange", display: false },
+  { id: 2, color: "#f3722c", label: "Orange", display: false },
   { id: 3, color: "#f8961e", label: "Peach", display: false },
-  { id: 4, color: "#f8961e", label: "Peach", display: true },
-  { id: 5, color: "#f9c74f", label: "Banana", display: true },
-  { id: 6, color: "#f9c74f", label: "Banana", display: true },
-  { id: 7, color: "#90be6d", label: "Grape", display: true },
-  { id: 8, color: "#90be6d", label: "Grape", display: true },
-  { id: 9, color: "#43aa8b", label: "Kiwi", display: true },
+  { id: 4, color: "#f8961e", label: "Peach", display: false },
+  { id: 5, color: "#f9c74f", label: "Banana", display: false },
+  { id: 6, color: "#f9c74f", label: "Banana", display: false },
+  { id: 7, color: "#90be6d", label: "Grape", display: false },
+  { id: 8, color: "#90be6d", label: "Grape", display: false },
+  { id: 9, color: "#43aa8b", label: "Kiwi", display: false },
   { id: 10, color: "#43aa8b", label: "Kiwi", display: false },
-  { id: 11, color: "#577590", label: "Plum", display: true },
-  { id: 12, color: "#577590", label: "Plum", display: true },
+  { id: 11, color: "#577590", label: "Plum", display: false },
+  { id: 12, color: "#577590", label: "Plum", display: false },
 ];
 
 function shuffle(array) {
@@ -56,33 +56,37 @@ function App() {
 
   useEffect(
     () => {
-      // was soll ich machen?
+      // Funktion des useEffect: was soll ich machen?
+
       // Wenn ich 10 Punkte habe,
       if (punkte === 10) {
         // dann möchte ich ausgeben: "Zeit abgelaufen"
-        // habe kein Ort / Variable um irgendeine Nachricht anzuzeigen
-        // -> muss die anlegen
+        // habe aber kein Ort / Variable um irgendeine Nachricht
+        // anzuzeigen -> muss die anlegen
+        // -> neuer State nachricht
         setNachricht("Zeit abgelaufen");
       }
     },
-    // was soll ich im Auge behalten? -> []
+    // Array des useEffect: was soll ich im Auge behalten? -> []
     [punkte]
   );
 
   useEffect(() => {
     if (aktuelleKarte !== null) {
       // Spielfeld ändern: setSpielfeld(???)
-      // 1.) Rausbekommen welcher Index vom cards-Array zur id passt.
+      // TODO: Rausbekommen welcher Index vom cards-Array zur id passt.
       const geänderteKarte = spielfeld.filter((karte, index) => {
         if (karte.id === aktuelleKarte) {
-          console.log(index);
+          console.log("Index der geklickten Karten:", index);
           return true;
         } else {
           return false;
         }
       });
+      // FIXME: Aus meiner alten Version, funktioniert aber ich
+      // glaube, das ist nicht wirklich gut gemacht:
       geänderteKarte[0].display = true;
-      setSpielfeld([...spielfeld], geänderteKarte[0]);
+      setSpielfeld([...spielfeld]);
 
       // mit setSpielfeld das Array an genau der Stelle (Index)
       //
